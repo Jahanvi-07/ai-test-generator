986531412498a2404aecb950f0d265b4e7730a24
Jahanvi Saini <jahanvisaini0307@gmail.com>
2026-03-15 10:17:26 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,6 +22,7 @@ const start = async () => {
     }
     
     app.listen(PORT, () => {
+      console.log("Mongo URI:", process.env.MONGO_URI);
       console.log(`✅ Server running at http://localhost:${PORT}`);
       console.log(`🔑 Gemini API Key: ${process.env.GEMINI_API_KEY ? '✅ Loaded' : '❌ Missing'}`);
     });
